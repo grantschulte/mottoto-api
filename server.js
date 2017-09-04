0017ef2d8eff6d32691753e5cbfdb1e57f8bc186
Grant Schulte <schulte.grant@gmail.com>
2017-09-04 12:31:57 -0400

    Uses process.env.port for PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 const config = require("./config");
-const port = config.getPort();
+const port = process.env.PORT || config.getPort();
 
 app.listen(port, () => {
   console.log(`Running on port ${port}.`);
