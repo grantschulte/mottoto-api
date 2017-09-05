9630dd65570643b1072e627b46238908a4bb46c5
Grant Schulte <schulte.grant@gmail.com>
2017-09-04 20:08:39 -0400

    Adds get author test

--- test/authors.test.js
+++ test/authors.test.js
@@ -7,6 +7,7 @@ const should = chai.should();
 const server = require("../server");
 
 const User = require("../src/routes/users/model");
+const Motto = require("../src/routes/mottos/model");
 
 chai.use(chaiHttp);
 
@@ -17,7 +18,7 @@ describe("Users", () => {
     });
   });
 
-  // GET /users
+  // GET /authors
 
   describe("GET /authors", () => {
     it("It should GET all authors.", done => {
@@ -29,6 +30,45 @@ describe("Users", () => {
           res.body.length.should.be.eql(0);
           done();
         });
+    });
+  });
+
+  // GET /authors/:handle
+
+  describe("GET /authors/:handle", () => {
+    it("It should GET an author by property handle.", done => {
+      let user = new User({
+        email: "picard@enterprise.com",
+        handle: "cptPicard",
+        password: "risa"
+      });
+
+      user.save((error, user) => {
+        let motto = new Motto({
+          text: "",
+          user: user._id
+        });
+
+        motto.save((error, motto) => {
+          user.motto = motto._id;
+
+          user.save((error, user) => {
+            chai.request(server)
+              .get(`/authors/${user.handle}`)
+              .end((error, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a("object");
+                res.body.should.have.property("_id");
+                res.body.should.have.property("handle");
+                res.body.should.have.property("motto");
+                res.body.motto.should.have.property("_id");
+                res.body.motto.should.have.property("text").eql("");
+                res.body.motto.should.have.property("user").eql(user.id);
+                done();
+              });
+          });
+        });
+      });
     });
   });
 });
